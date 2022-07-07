08f199cd9787ed80954ec49705ed8c3c5741c0b8
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-07-07 02:10:17 -0500

    more stuff

---
+++ migration_util.rb
@@ -0,0 +1,3 @@
+module MigrationUtil
+  Timestamp_format = "%Y%m%d%k%M%S";
+end
---
+++ validate_migration_version.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+require 'date'
+require 'optparse'
+require './migration_util.rb'
+
+
+def date_parser file, date
+	begin
+  		Date.strptime(date, MigrationUtil::Timestamp_format)
+	rescue => e
+		puts "The migration filename: '#{ file }' has an invalid name, ensure the date is following the format: '#{ MigrationUtil::Timestamp_format }'"
+  		puts "Exception Class: #{ e.class.name }"
+  		puts "Exception Message: #{ e.message }"
+  		puts "Exception Backtrace: #{ e.backtrace }"
+  		exit 1
+	end
+end
+
+
+Options = Struct.new(:dir)
+
+class Parser
+  def self.parse(options)
+    args = Options.new(options)
+
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: validate_migration_version.sh [options]"
+
+      opts.on("-dDIR", "--db-migration-dir=DIR", "Migrations directory") do |n|
+        args.dir = n
+      end
+
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
+
+      opts.on("-h", "--help", "Prints this help") do
+        puts opts
+        exit
+      end
+    end
+
+    opt_parser.parse!(options)
+    return args
+  end
+end
+
+arguments = ARGV
+
+if(ARGV.length == 0)
+  arguments = ["--help"]
+end
+
+options = Parser.parse arguments
+
+puts "Database migrations directory: '#{options.dir}'"
+
+files = Dir["#{options.dir}/*.sql"]
+
+files.each { |file| 
+
+	filename = File.basename(file, ".sql") 
+	migration_timestamp = filename.split("__")[0][1..-1]
+
+    date_parser(file, migration_timestamp)
+}
